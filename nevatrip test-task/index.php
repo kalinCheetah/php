229c1621c38da4e63df6d4bf634f11f047ce0540
kalinCheetah <93857539+kalinCheetah@users.noreply.github.com>
2024-11-02 16:20:51 +0500

    add description

--- nevatrip test-task/index.php
+++ nevatrip test-task/index.php
@@ -2,48 +2,56 @@
 require_once 'Database.php';
 require_once 'Order.php';
 
+/*
+ * Тут представлена логика работы программы по добавлению заказа в БД:
+ * сначала проверяем, что удалось забронировать. Подтверждаем и тогда заносим в БД запись.
+*/
+
 echo '<pre>';
 
-// создаем объекты, с которыми будем работать: БД и сам заказ. 
-// заказ конструируется из тестовых рандомизированных данных
-$db = new Database;
-$order = new Order(...testGerenerationFields()); 
-// флаги по дефолту == false
-$bookingIsOk = false;
-$approveBookingIsOk = false;
-
-// проверяем бронирование
-$message = $order->checkBooking();
-
-// пока не будет уникальный barcode продолжаем генерировать заново
-// шанс неуникальности предельно мал (13 знаков), но действую в соответствие с ТЗ
-while (isset($message['error']) && $message['error'] == 'barcode already exists') {
-    $error = true;
-    $message = $order->checkBooking($error);
-}
-if ($message['message'] == 'order successfully booked') {
-    echo 'Book is OK<br>';
-    $bookingIsOk = true;
-}
-
-// подтверждаем бронирование
-$message = $order->approveBooking();
-
-if ($message['message'] == 'order successfully approved') {
-    echo 'Approve is OK<br>';
-    $approveBookingIsOk = true;
-}
-// если из API пришла ошибка, выводим ее текст
-if (isset($message['error'])) {
-    print_r($message['error']);
-}
-
-// если бронирование && подтверждение бронирования прошли успешно, то добавляет заказ в БД
-if ($bookingIsOk && $approveBookingIsOk) {
-    $db->addOrder(...$order->getAllFields());
-}
+checkBookingAndAddToDatabase();
 
 echo '</pre>';
+function checkBookingAndAddToDatabase() {
+    // создаем объекты, с которыми будем работать: БД и сам заказ. 
+    // заказ конструируется из тестовых рандомизированных данных
+    $db = new Database;
+    $order = new Order(...testGerenerationFields()); 
+    // флаги по дефолту == false
+    $bookingIsOk = false;
+    $approveBookingIsOk = false;
+    
+    // проверяем бронирование
+    $message = $order->checkBooking();
+    
+    // пока не будет уникальный barcode продолжаем генерировать заново
+    // шанс неуникальности предельно мал (13 знаков), но действую в соответствие с ТЗ
+    while (isset($message['error']) && $message['error'] == 'barcode already exists') {
+        $error = true;
+        $message = $order->checkBooking($error);
+    }
+    if ($message['message'] == 'order successfully booked') {
+        echo 'Book is OK<br>';
+        $bookingIsOk = true;
+    }
+    
+    // подтверждаем бронирование
+    $message = $order->approveBooking();
+    
+    if ($message['message'] == 'order successfully approved') {
+        echo 'Approve is OK<br>';
+        $approveBookingIsOk = true;
+    }
+    // если из API пришла ошибка, выводим ее текст
+    if (isset($message['error'])) {
+        print_r($message['error']);
+    }
+    
+    // если бронирование && подтверждение бронирования прошли успешно, то добавляет заказ в БД
+    if ($bookingIsOk && $approveBookingIsOk) {
+        $db->addOrder(...$order->getAllFields());
+    }
+}
 
 /* функция генерирует случайные значения для конструирования заказа */
 function testGerenerationFields() {
